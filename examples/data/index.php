b0d0d3adc4816517d122cdd33142890714fb7d75
Dave Conroy <dave@tiredofit.ca>
2020-06-12 14:03:56 -0700

    Remove Examples and Logs

--- examples/data/index.php
+++
@@ -1,6 +0,0 @@
-  <html>
-  <title>Default Page</title>
-  <h2>Container is working</h2>
-  Congratulations! Your PHP-FPM image is working. You are seeing this because you don't have an index.php file in your /www/html directory.<br />
-  <?php phpinfo();?>
-  </html>
